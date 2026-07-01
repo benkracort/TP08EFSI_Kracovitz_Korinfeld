2cf586d07c319babe5dc74090d8db7101ee732be
Android Studio <devandroid@ort>
2026-07-01 08:28:55 -0300

    Fake comments + Web Expo

---
+++ src/data/fakeComments.js
@@ -0,0 +1,84 @@
+const fakeComments = [
+  {
+    id: "1",
+    username: "mia_22",
+    comment: "Qué lindo gatito 😻",
+  },
+  {
+    id: "2",
+    username: "lucas.dev",
+    comment: "Necesito adoptarlo ya.",
+  },
+  {
+    id: "3",
+    username: "catlover",
+    comment: "Las patitas son demasiado tiernas ❤️",
+  },
+  {
+    id: "4",
+    username: "sofia_87",
+    comment: "Hermosa foto 📸",
+  },
+  {
+    id: "5",
+    username: "nacho",
+    comment: "El mejor post del día.",
+  },
+  {
+    id: "6",
+    username: "julieta",
+    comment: "Más fotos de gatos, por favor.",
+  },
+  {
+    id: "7",
+    username: "martin",
+    comment: "Me alegró el día 😸",
+  },
+  {
+    id: "8",
+    username: "cami",
+    comment: "Le doy like mil veces.",
+  },
+  {
+    id: "9",
+    username: "franco",
+    comment: "Ese michi está viviendo su mejor vida.",
+  },
+  {
+    id: "10",
+    username: "agus",
+    comment: "¡Qué ojos más lindos! 😍",
+  },
+  {
+    id: "11",
+    username: "valen",
+    comment: "Mi gato hace la misma cara 😂",
+  },
+  {
+    id: "12",
+    username: "tomi",
+    comment: "Instagram necesitaba más gatos.",
+  },
+  {
+    id: "13",
+    username: "flor",
+    comment: "Demasiada ternura en una sola foto.",
+  },
+  {
+    id: "14",
+    username: "nico",
+    comment: "Le mando un abrazo virtual al michi 🐱",
+  },
+  {
+    id: "15",
+    username: "luli",
+    comment: "Este post merece millones de likes.",
+  },
+  {
+    id: "16",
+    username: "santi",
+    comment: "Definitivamente este es mi favorito.",
+  },
+];
+
+export default fakeComments;
